14dac39abfbecc82aada66fe79d87eb347c0fdf2
Mfullaty <mustaphaIbrahim37@gmail.com>
2026-03-04 12:30:28 +0000

    Reduce logo width and height

--- components/logo.tsx
+++ components/logo.tsx
@@ -14,7 +14,7 @@ export const Logo = (
             href="/"
             className={cn("flex items-center group dark:brightness-170 py-2", className)}
         >
-            <Image src="/assets/logos/logo.png" alt="Logo" width={150} height={100} />
+            <Image src="/assets/logos/logo.png" alt="Logo" width={130} height={80} />
         </Link>
     );
 }
