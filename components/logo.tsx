8878499748288307ee02ae283fed7864b26d0224
Mfullaty <mustaphaIbrahim37@gmail.com>
2026-03-04 12:29:25 +0000

    Add padding to logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -12,7 +12,7 @@ export const Logo = (
     return (
         <Link
             href="/"
-            className={cn("flex items-center gap-2 group dark:brightness-170", className)}
+            className={cn("flex items-center group dark:brightness-170 py-2", className)}
         >
             <Image src="/assets/logos/logo.png" alt="Logo" width={150} height={100} />
         </Link>
