f9d9389e3fdb37be0d9d33dae2ba14770b0d9b27
Mfullaty <mustaphaIbrahim37@gmail.com>
2026-03-04 12:35:23 +0000

    Final Logo Width and Height

--- components/logo.tsx
+++ components/logo.tsx
@@ -14,7 +14,7 @@ export const Logo = (
             href="/"
             className={cn("flex items-center group dark:brightness-170 py-2", className)}
         >
-            <Image src="/assets/logos/logo.png" alt="Logo" width={130} height={80} />
+            <Image src="/assets/logos/logo.png" alt="Logo" width={130} height={80} className="w-[130px] h-[80px] md:w-[160px] md:h-[120px] object-contain" />
         </Link>
     );
 }
